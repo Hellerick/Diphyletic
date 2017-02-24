a53e437677d9a9d7637ec3c25fe940f10d4f1e82
Hellerick <hellerickferlibay@gmail.com>
2017-02-24 17:59:32 +0700

    From Severboat

--- Diphyletic.py
+++ Diphyletic.py
@@ -1,12 +1,12 @@
 import random
 
 
-E, M, F = range(3) # empty, males, females
+E, M, F = range(3)  # empty, males, females
 
 
 def main():
     colony = Colony()
-    colony.random_fill([6,6])
+    colony.random_fill([6, 6])
     while True:
         colony.show()
         colony.evolve()
@@ -19,11 +19,11 @@ class Colony:
         self.generation = 0
 
     def random_fill(self, size):
-        C = []
+        colony_map = []
         for i in range(size[0]):
-            L = [random.choice([E, M, F]) for j in range(size[1])]
-            C = C + [L]
-        self.map = C
+            line = [random.choice([E, M, F]) for _ in range(size[1])]
+            colony_map += [line]
+        self.map = colony_map
 
     def show(self):
         print('Generation', self.generation)
@@ -32,15 +32,15 @@ class Colony:
         print()
 
     def check_borders(self):
-        while sum(self.map[0])==0:
+        while sum(self.map[0]) == 0:
             self.map = self.map[1:]
-        while sum(self.map[-1])==0:
+        while sum(self.map[-1]) == 0:
             self.map = self.map[:-1]
-        while sum([l[0] for l in self.map])==0:
+        while sum([l[0] for l in self.map]) == 0:
             self.map = [l[1:] for l in self.map]
-        while sum([l[-1] for l in self.map])==0:
+        while sum([l[-1] for l in self.map]) == 0:
             self.map = [l[:-1] for l in self.map]
-        self.map = [ [0]+l+[0] for l in self.map]
+        self.map = [[0]+l+[0] for l in self.map]
         empty_line = [0] * len(self.map[0])
         self.map = [empty_line] + self.map + [empty_line]
 
