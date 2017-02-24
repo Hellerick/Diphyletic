ff36b3c076cd3775b2c2e4948ab44ee9092ca22b
Hellerick <hellerickferlibay@gmail.com>
2017-02-24 15:11:31 +0700

    Initial commit

---
+++ Diphyletic.py
@@ -0,0 +1,53 @@
+import random
+
+
+E, M, F = range(3) # empty, males, females
+
+
+def main():
+    colony = Colony()
+    colony.random_fill([6,6])
+    while True:
+        colony.show()
+        colony.evolve()
+
+
+class Colony:
+
+    def __init__(self):
+        self.map = []
+        self.generation = 0
+
+    def random_fill(self, size):
+        C = []
+        for i in range(size[0]):
+            L = [random.choice([E, M, F]) for j in range(size[1])]
+            C = C + [L]
+        self.map = C
+
+    def show(self):
+        print('Generation', self.generation)
+        for line in self.map:
+            print(' '.join(' mf'[c] for c in line))
+        print()
+
+    def check_borders(self):
+        while sum(self.map[0])==0:
+            self.map = self.map[1:]
+        while sum(self.map[-1])==0:
+            self.map = self.map[:-1]
+        while sum([l[0] for l in self.map])==0:
+            self.map = [l[1:] for l in self.map]
+        while sum([l[-1] for l in self.map])==0:
+            self.map = [l[:-1] for l in self.map]
+        self.map = [ [0]+l+[0] for l in self.map]
+        empty_line = [0] * len(self.map[0])
+        self.map = [empty_line] + self.map + [empty_line]
+
+    def evolve(self):
+        self.check_borders()
+        self.generation += 1
+
+
+if __name__ == '__main__':
+    main()
