351c1bfd4117588883c46f9b6e38314621535811
Hellerick <hellerickferlibay@gmail.com>
2017-03-01 13:57:28 +0700

    From Severboat

--- Diphyletic.py
+++ Diphyletic.py
@@ -3,12 +3,26 @@ import random
 
 E, M, F = range(3)  # empty, males, females
 
-moves = [
-    [+1,  0],
-    [ 0, +1],
-    [-1,  0],
-    [ 0, -1],
-]
+rules = 2
+
+if rules == 1:
+    moves = [
+        [+1,  0],
+        [ 0, +1],
+        [-1,  0],
+        [ 0, -1],
+    ]
+elif rules == 2:
+    moves = [
+        [ 0, +1],
+        [+1, +1],
+        [+1,  0],
+        [+1, -1],
+        [ 0, -1],
+        [-1, -1],
+        [-1,  0],
+        [-1, +1],
+    ]
 
 
 def main():
@@ -70,38 +84,66 @@ class Colony:
     def evolve(self):
         self.check_borders()
         self.generation += 1
-        deathlist = []
-        birthlist = []
-        movelist = []
-        for i in range(1, len(self.map)-1):
-            for j in range(1, len(self.map[i])-1):
-                neighbors = self.neighbors(i, j)
-                if self.map[i][j] != 0 and neighbors[0] in [0,4]:
-                    deathlist += [[i,j]]
-                if (
-                        self.map[i][j] == 0 and
-                        neighbors[M] == 1 and
-                        neighbors[F] == 1
-                    ):
-                    paircode = ''.join([str(self.map[i+m[0]][j+m[1]]) for m in moves])
-                    paircode = [c for c in paircode if c != '0']
-                    gender = int(paircode[0])
-                    birthlist += [[i,j,gender]]
-                if self.map[i][j] != 0 and neighbors[0] in [3,2]:
-                    moved = False
-                    for m in moves:
-                        if not moved:
-                            new_neighbors = self.neighbors(i+m[0], j+m[1])
-                            if new_neighbors[0] in [1,2] and new_neighbors[0] < neighbors[0]:
-                                movelist += [[i,j,i+m[0],j+m[1]]]
-                                moved = True
-        for b in birthlist:
-            self.map[b[0]][b[1]] = b[2]
-        for d in deathlist:
-            self.map[d[0]][d[1]] = 0
-        for m in movelist:
-            self.map[m[2]][m[3]] = self.map[m[0]][m[1]]
-            self.map[m[0]][m[1]] = 0
+
+        if rules == 1:
+            deathlist = []
+            birthlist = []
+            movelist = []
+            for i in range(1, len(self.map)-1):
+                for j in range(1, len(self.map[i])-1):
+                    neighbors = self.neighbors(i, j)
+                    if self.map[i][j] != 0 and neighbors[0] in [0,4]:
+                        deathlist += [[i,j]]
+                    if (
+                            self.map[i][j] == 0 and
+                            neighbors[M] == 1 and
+                            neighbors[F] == 1
+                        ):
+                        paircode = ''.join([str(self.map[i+m[0]][j+m[1]]) for m in moves])
+                        paircode = [c for c in paircode if c != '0']
+                        gender = int(paircode[0])
+                        birthlist += [[i,j,gender]]
+                    if self.map[i][j] != 0 and neighbors[0] in [3,2]:
+                        moved = False
+                        for m in moves:
+                            if not moved:
+                                new_neighbors = self.neighbors(i+m[0], j+m[1])
+                                if new_neighbors[0] in [1,2] and new_neighbors[0] < neighbors[0]:
+                                    movelist += [[i,j,i+m[0],j+m[1]]]
+                                    moved = True
+            for b in birthlist:
+                self.map[b[0]][b[1]] = b[2]
+            for d in deathlist:
+                self.map[d[0]][d[1]] = 0
+            for m in movelist:
+                self.map[m[2]][m[3]] = self.map[m[0]][m[1]]
+                self.map[m[0]][m[1]] = 0
+
+        if rules == 2:
+            deathlist = []
+            birthlist = []
+            for i in range(1,len(self.map)-1):
+                for j in range(1,len(self.map[i])-1):
+                    neighbors = self.neighbors(i, j)
+                    if self.map[i][j]>0 and (neighbors[0]<2 or neighbors[0]>3):
+                        deathlist += [[i,j]]
+                    if self.map[i][j]==0 and neighbors[1]==3 and neighbors[1]!=2:
+                        birthlist += [[i,j,1]]
+                    if self.map[i][j]==0 and neighbors[2]==3 and neighbors[1]!=2:
+                        birthlist += [[i,j,2]]
+                    if neighbors == [2,1,1]:
+                        index = [0,0]
+                        for n,m in enumerate(moves):
+                            if self.map[i+m[0]][j+m[1]] == M:
+                                index[0] = n
+                            if self.map[i+m[0]][j+m[1]] == F:
+                                index[1] = n
+                        birthlist += [[i, j, (index[0]-index[1])%2+1]]
+            for b in birthlist:
+                self.map[b[0]][b[1]] = b[2]
+            for d in deathlist:
+                self.map[d[0]][d[1]] = 0
+
         self.image = '\n'.join([' '.join([' mf'[j] for j in i]) for i in self.map])
 
 if __name__ == '__main__':
